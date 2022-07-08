46272c10b1981375d530041e08dc0492bb230e9e
tmxn <nuriyasov@gmail.com>
2022-07-09 01:09:45 +0500

    Finished sliding window minimum. Gave up and just used queue<pair<int, int>>

--- projects/helloworld/aa_sliding_window_minimum.cpp
+++ projects/helloworld/aa_sliding_window_minimum.cpp
@@ -41,7 +41,7 @@ int main()
 {
     std::vector<int> array = {2, 1, 4, 5, 3, 4, 1, 2};
 
-    std::queue<int> queue;
+    std::queue<std::pair<int, int>> queue;
 
     const int SLIDING_WINDOW_SIZE = 4;
     
@@ -50,34 +50,35 @@ int main()
 
     std::cout << "WR " << window_r << " WL " << window_l << endl;
 
+
+    //Pre-fill the queue first, using the core idea of the sliding window minimum algorithm
     for (int i = 0; i < SLIDING_WINDOW_SIZE; i++)
     {
-        while (queue.size() && queue.front() > array[i])
+        while (queue.size() && queue.front().first > array[i])
             queue.pop();
         
-        queue.push(array[i]);
+        queue.push(std::make_pair(array[i], i));
     }
 
     std::cout << "Container " << queue.size() << endl;
 
+    //Now the main part of the algo
     while (window_r < (int)array.size())
     {
+        window_l = window_r - (SLIDING_WINDOW_SIZE - 1);
         std::cout << "L IS " << array[window_l] << endl;
         //since the window moved one to the right, remove first element
-        if (queue.front() == array[window_l])
+        if (queue.front().second < window_l)
             queue.pop();
-        
-        window_l = window_r - (SLIDING_WINDOW_SIZE - 1);
 
-        
         //now remove all elements from the left side that are larger than the new element
-        while (queue.size() && queue.front() > array[window_r])
+        while (queue.size() && queue.front().first >= array[window_r])
             queue.pop();
         
         std::cout << "R IS " << array[window_r] << endl;
-        queue.push(array[window_r]);
+        queue.push(std::make_pair(array[window_r], window_r));
 
-        std::cout << "Smallest element is " << queue.front() << endl;
+        std::cout << "Smallest element is " << queue.front().first << endl;
         window_r++;
     }
 }
