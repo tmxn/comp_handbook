c45fc8a434fd1897d637e974fd975013aae6658f
Timur Nuriyasov <timur.n@scopicsoftware.com>
2022-07-13 22:43:13 +0500

    dummy file for range query minimum sparse table

---
+++ projects/helloworld/rq_min_sparse_table.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+template<typename T>
+void dbgprint(bool debug, const T& entries)
+{
+    if (debug)
+    {
+        std::cout << "{";
+        for (auto & it : entries)
+        {
+            std::cout << it.first << "-" << it.second << " ";
+        }
+        std::cout << "}";
+    }
+}
+
+template<typename T>
+void printSequence(const T& sequence)
+{
+    std::cout << "{";
+    for (auto & it : sequence)
+    {
+        std::cout << it << " ";
+    }
+    std::cout << "} ";
+}
+
+int main()
+{
+    std::vector<int> items = {1, 3, 4, 8, 6, 1, 4, 2};
+    auto prefixArr = buildPreixSumArray(items);
+    std::cout << "Sum 3, 6 is " << calcSumStatic(prefixArr, 3, 6) << endl;
+}
