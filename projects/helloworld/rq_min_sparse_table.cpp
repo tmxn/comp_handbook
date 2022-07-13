d718c4900b93bbb4f01167f0e462b409c57917ed
tmxn <nuriyasov@gmail.com>
2022-07-14 01:09:53 +0500

    ongoing work on range minimum query sparse table

--- projects/helloworld/rq_min_sparse_table.cpp
+++ projects/helloworld/rq_min_sparse_table.cpp
@@ -31,15 +31,38 @@ void printSequence(const T& sequence)
 {
     std::cout << "{";
     for (auto & it : sequence)
-    {
+    {   
         std::cout << it << " ";
     }
     std::cout << "} ";
 }
 
+std::vector<std::vector<int>> buildSparseTable(const std::vector<int>& array)
+{
+    std::vector<std::vector<int>> res;
+
+    int size = (int)array.size();
+    //first item is just the array itself
+    res.push_back(array);
+
+    //to build other items, find out how many steps we need
+    int maxpower = floor(log2(array.size()));
+    std::cout << "Size is " << array.size() << " Steps are " << maxpower << endl;
+
+    for (int power = 0; power <= maxpower; power++)
+    {
+        int step = 1 << power;
+    }
+
+    return res;
+}
+
 int main()
 {
     std::vector<int> items = {1, 3, 4, 8, 6, 1, 4, 2};
-    auto prefixArr = buildPreixSumArray(items);
-    std::cout << "Sum 3, 6 is " << calcSumStatic(prefixArr, 3, 6) << endl;
+    auto sparseTable = buildSparseTable(items);
+    for (auto & it : sparseTable)
+    {
+        printSequence(it);
+    }
 }
