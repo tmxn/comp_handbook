2db412c9566cf18dd3e9a70dee946256379f1c5a
tmxn <nuriyasov@gmail.com>
2022-07-18 01:32:33 +0600

    finished implementing static range minimum query with sparse table

--- projects/helloworld/rq_min_sparse_table.cpp
+++ projects/helloworld/rq_min_sparse_table.cpp
@@ -61,18 +61,18 @@ std::vector<std::vector<int>> buildSparseTable(const std::vector<int>& array)
 
     //find out how many steps we need
     int maxpower = floor(log2(array.size()));
-    std::cout << "Size is " << array.size() << " Steps are " << maxpower << endl;
+    //std::cout << "Size is " << array.size() << " Steps are " << maxpower << endl;
 
     for (int power = 0; power <= maxpower; power++)
     {
         res.push_back(std::vector<int>());
         int step = 1 << power;
-        std::cout << "Step " << step << endl;
+        //std::cout << "Step " << step << endl;
         int leftInd = 0;
         int rightInd = leftInd + step - 1;
         while (rightInd < size)
         {
-            std::cout << "Considering pair " << leftInd << " " << rightInd << endl;
+            //std::cout << "Considering pair " << leftInd << " " << rightInd << endl;
             int minimum = 0;
             //
             if (res.size() == 1)
@@ -81,7 +81,12 @@ std::vector<std::vector<int>> buildSparseTable(const std::vector<int>& array)
             {
                 //
                 int hafu = leftInd + step / 2;
-                //
+                //dynamic programming. Use previous result to calculate next result efficiently
+                //The idea is that a minimum in range of length 4 is minimum of two ranges of lengths 2
+                //If indexes for range length 4 are (0..3) then we have to consider previous ranges
+                //with indexes (0..1) and (2..3). These minimums will be stored in our previous res row
+                //with indexes 0 and 2. At index 1 will be stored the minimum of range (1..2), which we don't need
+                //Note that 2 happens to be 4 (our current range length) divided by 2
                 minimum = std::min(res[res.size() - 2][leftInd], res[res.size() - 2][hafu]);
             }
             res.back().push_back(minimum);
@@ -95,16 +100,31 @@ std::vector<std::vector<int>> buildSparseTable(const std::vector<int>& array)
 
 int rangeMinimumQuerySmart(const std::vector<std::vector<int>>& sparseTable, int from, int to)
 {
-    return 0;
+    //Handle special case when log2 function fails
+    if (from == to)
+        return sparseTable[0][from];
+    //builtin_clz is unreliable here (bit shifting could be useful here - to find the most significant bit)
+    //Instead just use good ol' log2 to find the most significant bit
+    int p = (int)floor(log2(to - from));
+    
+    return std::min(sparseTable[p][from], sparseTable[p][to - (1 << p) + 1]);
 }
 
 int main()
 {
     std::vector<int> items = {1, 3, 4, 8, 6, 1, 4, 2};
-    auto sparseTable = buildSparseTable(items);
-    for (auto & it : sparseTable)
     {
-        printSequence(it);
+        auto sparseTable = buildSparseTable(items);
+        for (auto & it : sparseTable)
+        {
+            printSequence(it);
+        }
+        std::cout << endl;
+
+        int ms = rangeMinimumQuerySmart(sparseTable, 1, 5);
+        int md = rangeMinimumQueryDumb(items, 1, 5);
+        
+        std::cout << "MS is " << ms << " md is " << md << endl;
     }
 
     std::cout << "Perform a randomized full test ? (y/n)\n";
@@ -121,6 +141,8 @@ int main()
     std::uniform_int_distribution<> indexDistr(0, 99); // define the range
 
     items.resize(100);
+
+    int successes = 0;
     for (int c = 0; c < 100; c++)
     {
         //Generate a random list
@@ -144,7 +166,11 @@ int main()
                 std::cout << "Failed test ";
                 printSequence(items);
                 std::cout << " from " << rFrom << " to " << rTo << endl;
+            } else
+            {
+                successes++;
             }
         }
     }
+    std::cout << "Testing finished with " << successes << " successes!\n";
 }
