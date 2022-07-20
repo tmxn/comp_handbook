1fcda276a6ce55ef86308b9be4d75abcb9c8d0ac
Timur Nuriyasov <timur.n@scopicsoftware.com>
2022-07-21 02:56:45 +0500

    implemented fenwick tree range sum query

--- projects/helloworld/rq_fenwick_tree.cpp
+++ projects/helloworld/rq_fenwick_tree.cpp
@@ -43,7 +43,7 @@ int rangeSumQueryDumb(const std::vector<int>& array, int from, int to)
     int result = 0;
     for (int i = from; i <= to; i++)
     {
-        result += array[from];
+        result += array[i];
     }
     return result;
 }
@@ -53,35 +53,68 @@ int largestPowerOfTwoThatDividesN(int n)
     return n & (~(n - 1)); 
 }
 
-void fenwickTreeAdd(int index, int value)
+//updates the tree, modifies logN values in total
+void fenwickTreeAdd(std::vector<int>& fenwickTree, int index, int value)
 {
-
+    while (index <= fenwickTree.size())
+    {
+        //Update the initial index always
+        fenwickTree[index] += value;
+        //Now go to other affected indexes
+        index += largestPowerOfTwoThatDividesN(index); //1->2->4->8; odd -> even, even goes to nearest power of two (eventually)
+    }
 }
 
 //Builds a fenwick tree from a given array of values
 std::vector<int> buildFenwickTree(const std::vector<int>& array)
 {
-    std::vector<int> res;
+    //Prepare the storage for the tree. Tree is indexed from 1, so has size larger than the input array by 1
+    std::vector<int> res(array.size() + 1, 0);
     
     //Tree[k] = rangeSum(k-p(k) + 1, k)
     //Each element in the tree-array stores a sum from index k-p(k)+1 up to k
     //Numbers that are powers of two store the longest ranges. Odd numbers store just 1 sum
+
+    //Will build the full tree in NlogN time
+    for (int i = 0; i < array.size(); i++)
+    {
+        fenwickTreeAdd(res, i + 1, array[i]);
+    }
     
+    return res;
+}
+
+//gets a range sum in the fenwick tree from index 1 to the specified in logN time
+int fenwickTreeGet(const std::vector<int>& fenwickTree, int to)
+{
+    int res = 0;
+    while (to > 0)
+    {
+        res += fenwickTree[to];
+        to -= largestPowerOfTwoThatDividesN(to);
+    }
     return res;
 }
 
 int rangeSumQuerySmart(const std::vector<int>& fenwickTree, int from, int to)
 {
-    return 0;
+    return fenwickTreeGet(fenwickTree, to + 1) - fenwickTreeGet(fenwickTree, from);
 }
 
 int main()
 {
+    //~~~Driver code~~~
     std::cout << "largest power of two that divides 48 is " << largestPowerOfTwoThatDividesN(48) << endl;
     std::cout << "largest power of two that divides 6 is " << largestPowerOfTwoThatDividesN(6) << endl;
+    std::cout << "largest power of two that divides 10 is " << largestPowerOfTwoThatDividesN(10) << endl;
     std::vector<int> items = {1, 3, 4, 8, 6, 1, 4, 2};
-    
 
+    printSequence(buildFenwickTree(items));
+    
+    std::cout << "Sum from 1 to 4 is " << rangeSumQuerySmart(buildFenwickTree(items), 1, 4) << " and dum sum is " << rangeSumQueryDumb(items, 1, 4) << endl;
+
+
+    //~~~Randomized test~~~
     std::cout << "Perform a randomized full test ? (y/n)\n";
 
     char yn = 'n';
@@ -104,6 +137,8 @@ int main()
         for (auto & it : items)
             it = distr(gen);
 
+        auto fenwickTree = buildFenwickTree(items);
+
         //Make a bunch of queries
         for (int mq = 0; mq < 100; mq++)
         {
@@ -111,9 +146,9 @@ int main()
             int rFrom = indexDistr(gen);
             int rTo = indexDistr(gen);
             if (rTo < rFrom) std::swap(rFrom, rTo);
-            int minTrue = rangeSumQueryDumb(items, rFrom, rTo);
-            int minTest = rangeSumQuerySmart(items, rFrom, rTo);
-            if (minTrue != minTest)
+            int sumTrue = rangeSumQueryDumb(items, rFrom, rTo);
+            int sumTest = rangeSumQuerySmart(fenwickTree, rFrom, rTo);
+            if (sumTrue != sumTest)
             {
                 std::cout << "Failed test ";
                 printSequence(items);
