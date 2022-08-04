76aca4c7b8fccfa2bac1558aeedce8b15fec45c4
tmxn <nuriyasov@gmail.com>
2022-08-05 01:57:00 +0500

    started on dp_optimal selection

---
+++ projects/helloworld/bm_dp_optimal_selection.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <queue>
+#include <random>
+
+using namespace std;
+
+template<typename T>
+void dbgprint(bool debug, const T& entries)
+{
+    if (debug)
+    {
+        std::cout << "{";
+        for (auto & it : entries)
+        {
+            std::cout << it.first << "-" << it.second << " ";
+        }
+        std::cout << "}\n";
+    }
+}
+
+template<typename T>
+void printSequence(const T& sequence)
+{
+    std::cout << "{";
+    for (auto & it : sequence)
+    {   
+        std::cout << it << " ";
+    }
+    std::cout << "}\n";
+}
+
+int optimal(const std::vector<std::vector<int>>& prices)
+{
+    
+}
+
+int main()
+{
+    //columns are days, rows are products, cells are price values
+    std::vector<std::vector<int>> prices = {
+        {6, 9, 5, 2, 8, 9, 1, 6},
+        {8, 2, 6, 2, 7, 5, 7, 2},
+        {5, 3, 9, 7, 3, 5, 1, 4}
+    };
+    
+}
